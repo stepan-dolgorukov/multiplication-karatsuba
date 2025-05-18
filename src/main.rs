39458900cfb3a73ad74560757bfef9c6ecfd8c47
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-05-18 23:20:56 +0500

    Определение разбиения; определение функции, вычисляющей разбиение числа

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ fn calculate_length(value: &BigUint) -> u32 {
   value.to_str_radix(10).len() as u32
 }
 
+struct Split {
+  low: BigUint,
+  high: BigUint
+}
+
+fn calculate_split(value: &BigUint) -> Split {
+  let length: u32 = calculate_length(value);
+  let divisor: BigUint = BigUint::from(10u8).pow(length / 2);
+  Split { low: value % &divisor, high: value / &divisor }
+}
+
 fn main() {
   println!("{}", calculate_length(&BigUint::from(2735u64)));
 }
