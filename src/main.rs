243159eed2dbac10710ccd2ed14d3d20c97f38cf
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-05-18 23:07:41 +0500

    Определение функции вычисления длины числа в 10-ричной системе счисления

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
+use num_bigint::BigUint;
+
+fn calculate_length(value: &BigUint) -> u32 {
+  value.to_str_radix(10).len() as u32
+}
+
 fn main() {
+  println!("{}", calculate_length(&BigUint::from(2735u64)));
 }
